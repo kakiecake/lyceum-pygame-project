ae4c3dbee655115df0bad75b1d3da22120890999
4siNL <defflow6@gmail.com>
2021-01-08 14:46:09 +0300

    Реализован класс Board
    Доработана логика. Уже можно играть

--- classes.py
+++ classes.py
@@ -2,12 +2,7 @@ import pygame
 import os
 import sys
 import random
-from framework import event_handler, main
-
-balls_group = pygame.sprite.Group()
-bricks_group = pygame.sprite.Group()
-boards_group = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
+from framework import event_handler
 
 
 def load_image(name):
@@ -19,26 +14,93 @@ def load_image(name):
     return image
 
 
+class Board:
+    def __init__(self, size):
+        width, height = size
+        self.balls_group = pygame.sprite.Group()
+        self.bricks_group = pygame.sprite.Group()
+        self.platforms_group = pygame.sprite.Group()
+        self.horizontal_barriers = pygame.sprite.Group()
+        self.vertical_barriers = pygame.sprite.Group()
+        self.horizontal_barriers.add(Barrier(0, 0, width, 0))
+        self.horizontal_barriers.add(Barrier(0, height, width, height))
+        self.vertical_barriers.add(Barrier(0, 0, 0, height))
+        self.vertical_barriers.add(Barrier(width, 0, width, height))
+        self.platform = Platform((400, 470))
+        self.platforms_group.add(self.platform)
+        self.bricks_group.add(*[Brick((5 + 105 * i, 5 + 35 * j)) for i in range(10)
+                           for j in range(4)])
+        self.ball = Ball((random.randint(0, 930), 450))
+        self.balls_group.add(self.ball)
+
+    def update(self):
+        self.ball.update()
+        if pygame.sprite.spritecollideany(self.ball, self.horizontal_barriers):
+            self.ball.on_horizontal_collision()
+        if pygame.sprite.spritecollideany(self.ball, self.vertical_barriers):
+            self.ball.on_vertical_collision()
+        for brick in self.bricks_group:
+            if pygame.sprite.spritecollideany(brick, self.balls_group):
+                pygame.sprite.spritecollide(self.ball, self.bricks_group, True)
+                self.ball.on_brick_collision(brick)
+                break
+        for platform1 in self.platforms_group:
+            if pygame.sprite.spritecollideany(platform1, self.balls_group):
+                self.ball.on_brick_collision(platform1)
+
+    def render(self, screen):
+        self.balls_group.draw(screen)
+        self.bricks_group.draw(screen)
+        self.platforms_group.draw(screen)
+
+    @event_handler(pygame.KEYDOWN, use_event=True)
+    def on_keydown(self, event):
+        if event.key == pygame.K_LEFT:
+            self.platform.move_left()
+        elif event.key == pygame.K_RIGHT:
+            self.platform.move_right()
+
 class Ball(pygame.sprite.Sprite):
     def __init__(self, pos):
-        super().__init__(balls_group, all_sprites)
+        super().__init__()
         self.image = load_image("ball.png")
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
         self.vx = random.randint(-5, 5)
         self.vy = random.randrange(-5, -1)
+        while self.vx == 0:
+            self.vx = random.randint(-5, 5)
 
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
 
-    def render(self):
-        pass
+    def on_brick_collision(self, sprite):
+        if self.vx > 0:
+            x = self.rect.right - sprite.rect.left
+        else:
+            x = sprite.rect.right - self.rect.left
+        if self.vy > 0:
+            y = self.rect.bottom - sprite.rect.top
+        else:
+            y = sprite.rect.bottom - self.rect.top
+        if abs(x - y) < 5:
+            self.vx, self.vy = -self.vx, -self.vy
+        elif x > y:
+            self.vy = -self.vy
+        elif y > x:
+            self.vx = -self.vx
+
+    def on_vertical_collision(self):
+        self.vx = -self.vx
+
+    def on_horizontal_collision(self):
+        self.vy = -self.vy
 
 
 class Brick(pygame.sprite.Sprite):
     def __init__(self, pos):
-        super().__init__(bricks_group, all_sprites)
+        super().__init__()
         self.image = load_image("brick.png")
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
@@ -47,24 +109,31 @@ class Brick(pygame.sprite.Sprite):
     def update(self):
         pass
 
-    def render(self):
-        pass
 
-
-class Board(pygame.sprite.Sprite):
+class Platform(pygame.sprite.Sprite):
     def __init__(self, pos):
-        super().__init__(boards_group, all_sprites)
-        self.image = load_image("board.png")
+        super().__init__()
+        self.image = load_image("platform.png")
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
-    @event_handler(pygame.KEYDOWN, use_event=True)
-    def update(self, event):
-        if event.key == pygame.K_LEFT:
-            self.rect = self.rect.move(-5, 0)
-        elif event.key == pygame.K_RIGHT:
-            self.rect = self.rect.move(5, 0)
-
-    def render(self):
+    def update(self):
         pass
+
+    def move_left(self):
+        self.rect = self.rect.move(-10, 0)
+
+    def move_right(self):
+        self.rect = self.rect.move(10, 0)
+
+
+class Barrier(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__()
+        if x1 == x2:
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from menu import Menu
 from leaderboard import Leaderboard
 from framework import SceneManager, loop
+from classes import Board
 import pygame
 
 if __name__ == '__main__':
     pygame.init()
     pygame.font.init()
+    size = (950, 500)
 
     scene_manager = SceneManager()
-    menu = Menu(go_to_leaderboard=lambda: scene_manager.switch_to('leaderboard'))
+    menu = Menu(go_to_leaderboard=lambda: scene_manager.switch_to('leaderboard'),
+                go_to_level=lambda: scene_manager.switch_to('board'))
     leaderboard = Leaderboard()
+    board = Board(size)
 
+    scene_manager.add_scene('board', board)
     scene_manager.add_scene('menu', menu)
     scene_manager.add_scene('leaderboard', leaderboard)
     scene_manager.switch_to('menu')
 
-    size = (1000, 1000)
     screen = pygame.display.set_mode(size)
     loop(scene_manager, screen)
